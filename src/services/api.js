8ea023367ae9c7e7bf6d441213b18af303a96d33
Kartikey pandey <kartikey224252@gmail.com>
2026-02-28 09:17:04 +0530

    parser

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 // API Base URL
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://jewel-kxpf.onrender.com/api';
 
 // Get stored admin password
 const getAuthHeader = () => {
